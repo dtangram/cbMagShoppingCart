1d02f1f7545db31e0e86190120d7241994f9e309
dtangram <dtangram@yahoo.com>
2025-11-13 17:12:06 -0500

    Reverted root server and package files.

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-import express, { Request, Response } from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import createDebug from 'debug';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require('express');
+const path = require('path');
+const createDebug = require('debug');
 
 const log = createDebug('reactjs:logging');
 
 const app = express();
-const port: number = parseInt(process.env.PORT || '3000', 10);
+const port = parseInt(process.env.PORT || '5000', 10);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'reactjs/build')));
 
-app.get('*', (req: Request, res: Response) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'reactjs/build', 'index.html'));
 });
 
